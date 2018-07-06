3cfa22f1c9c382537950dc982b4ebc1239f13cb6
sree <dsreekantr211@gmail.com>
2018-07-06 10:17:41 +0530

    next letter added

--- Desktop/python/hello.py
+++ Desktop/python/hello.py
@@ -2,4 +2,4 @@ print("how many kilometers you have ride")
 kms = input()
 miles=float(kms)/1.60934
 miles = round(miles, 2)
-print(f"you have ride {miles} miles")
+print(f"your {kms} kms ride  is equal  {miles} miles")
