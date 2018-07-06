f9eb0b7c18cd7354ad606b5bb48836ab9303b16a
sree <dsreekantr211@gmail.com>
2018-07-06 21:50:56 +0530

    area calculator

--- Desktop/python/sree.py
+++ Desktop/python/sree.py
@@ -1,1 +1,12 @@
-print("new line")
+
+#print("how many meteres you have travelled")
+#kms = input()
+#meters = int(kms)*1000
+#print(f"i have travlleld {meters} meters today ")
+
+print("give height and breadth of rectangle")
+length = input("give length in meters")
+breadth = input("give breadth in meters" )
+area = 2*(int(length)*int(breadth))
+print(f"area of the square is {area}")
+
