132fc8f03d7d89b43661aa8e82e7bb57286cbee8
sree <dsreekantr211@gmail.com>
2018-07-06 22:09:01 +0530

    if statement

--- Desktop/python/sree.py
+++ Desktop/python/sree.py
@@ -4,9 +4,17 @@
 #meters = int(kms)*1000
 #print(f"i have travlleld {meters} meters today ")
 
-print("give length and breadth of rectangle")
-length = input("give length in meters")
-breadth = input("give breadth in meters" )
-area = 2*(int(length)*int(breadth))
-print(f"area of the square is {area}")
+#print("give length and breadth of rectangle")
+#length = input("give length in meters")
+#breadth = input("give breadth in meters" )
+#area = 2*(int(length)*int(breadth))
+#print(f"area of the square is {area}")
 
+print("print age of girl")
+age = input("age")
+if int(age)>18:
+    print("she is major")
+elif int(age)<18:
+    print("she is minor")
+else:
+    print("give value  greater than or less than 18")
